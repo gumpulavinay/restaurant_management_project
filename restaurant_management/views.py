a37a779af4424b818cafb0e43533682ef07d042d
gumpulavinay <gumpulavinay2003@gmail.com>
2025-08-28 15:16:57 +0530

    38313

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -4,4 +4,4 @@ from .models import Restaurant
 
 def homepage(request):
     restaurant = Restaurant.objects.first()
-    return render (request,'home.html, {'restaurant_name':restaurant.name if restaurant else "Restaurant"}')
+    return render (request,'homepage.html, {"phone_number": restaurant.phone}')
