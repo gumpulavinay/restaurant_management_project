30780f2d69d2db9f16f3f1b10633d221478c4a0a
gumpulavinay <gumpulavinay2003@gmail.com>
2025-08-28 14:33:20 +0530

    38310

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def menu_items(request):
+    items = [
+        {"name": "pizza","price": 250},
+        {"name":"Burger","price":120},
+        {"name":"sandwich","price":100},
+    ]
+    return render (request, "menu_items.html",{"items":items})
