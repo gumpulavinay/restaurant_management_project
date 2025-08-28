26c3fb205944b9e8241c14d7a357093a0ce52ec4
gumpulavinay <gumpulavinay2003@gmail.com>
2025-08-28 15:07:48 +0530

    38312

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contact_us(request):
+    contact_info = {
+        "email":"support@example.com",
+        "phone": "+91 98765 43210",
+        "address": "123, Main Street,Hyderabad,India"
+    }
+    return render(request,"contact_us.html",{"contact":contact_info})
